c6d268f6477d681ae85703eba88b782e45009a5c
Renan Souza <s.renan@outlook.com>
2022-02-24 20:23:27 -0300

    fix : ?

--- desafio19.sql
+++ desafio19.sql
@@ -1,1 +1,1 @@
-SELECT COUNT(*) FROM orders WHERE (employee_id = 5 OR employee_id = 6) AND shipper_id = 2;
+SELECT COUNT(*) AS 'orders_count' FROM orders WHERE (employee_id = 5 OR employee_id = 6) AND shipper_id = 2;
